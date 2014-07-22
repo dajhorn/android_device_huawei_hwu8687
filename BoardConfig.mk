fed04d5ba24ea0ced24821dc840ca1907e6ba156
Darik Horn <dajhorn@vanadac.com>
2014-07-22 19:50:31 -0400

    Integrate msm7x727a-common repository.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,9 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/huawei/hwu8687/BoardConfigVendor.mk
 
+# Inherit from the common msm7x27a definitions.
+include device/huawei/msm7x27a-common/BoardConfigCommon.mk
+
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -23,10 +26,12 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x1E848200
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_BOOTLOADER_BOARD_NAME := hwu8687
-TARGET_BOARD_PLATFORM := msm7k
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := false
 TARGET_CPU_VARIANT := generic
 TARGET_KERNEL_CONFIG := hwu8687_defconfig
 TARGET_NO_BOOTLOADER := true
+TARGET_PROVIDES_LIBAUDIO := true
+TARGET_OTA_ASSERT_DEVICE := u8687,hwu8687
 #TARGET_PREBUILT_KERNEL := device/huawei/hwu8687/kernel
